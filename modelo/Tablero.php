4c320bfed2243ae70a297ef0042be8abd3152889
mariof99 <marioferestevez@gmail.com>
2022-10-10 09:49:30 +0200

    esqueleto planteado

---
+++ modelo/Tablero.php
@@ -0,0 +1,7 @@
+<?php
+
+class Tablero {
+    private $contenido;
+    private $mostrado;
+    
+}
---
+++ modelo/Usuario.php
@@ -0,0 +1,11 @@
+<?php
+
+class Usuario {
+    private $nickname;
+    private $passwd;
+
+    function __construct($nickname, $passwd) {
+        $this->nickname = $nickname;
+        $this->nickname = $passwd;
+    }
+}
