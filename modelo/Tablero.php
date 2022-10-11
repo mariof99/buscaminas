373ac157653bf48ac7c5bb1117f1d0faf863c553
mariof99 <marioferestevez@gmail.com>
2022-10-11 23:34:49 +0200

    generarTablero y marcarPosicion terminadas

--- modelo/Tablero.php
+++ modelo/Tablero.php
@@ -1,14 +1,29 @@
 <?php
 
-class Tablero {
-    private $usr;
-    private $contenido;
+class Tablero { // tabOculto (> 1 ==> nº minas cerca / 0 ==> nada / -1 ==> mina)
+    private $tabOculto;
     private $mostrado;
+    private $partidaPerdida;
 
-    function __construct($usr, &$contenido, &$mostrado) {
-        $this->usr = $usr;
-        $this->contenido = $contenido;
+    function __construct(&$tabOculto, &$mostrado) {
+        $this->tabOculto = $tabOculto;
         $this->mostrado = $mostrado;
+        $this->partidaPerdida = false;
     }
     
+    private function darPutaso($pos) {
+        switch (true) {
+            case $this->tabOculto[$pos] >= 1:
+                $this->mostrado[$pos] = true;
+                break;
+
+            case $this->tabOculto[$pos] == 0:
+                $this->partidaPerdida = true;
+
+            
+            default:
+                # code...
+                break;
+        }
+    }
 }
